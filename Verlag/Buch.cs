39d09e151d208cc059d12e884c9e3660da731948
Berkant <dernamentyp@protonmail.com>
2023-01-23 10:25:00 +0100

    first commit

---
+++ Verlag/Buch.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+
+        public string Autor
+        {
+            set
+            {
+                autor = value;
+            }
+            get
+            {
+                return autor;
+            }
+        }
+
+            public string Titel
+        {
+            get
+            {
+                return titel;
+            }
+        }
+
+        public int Auflage
+        {
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Auflage darf nicht kleiner als 1 sein.");
+                }
+                else
+                {
+                    this.auflage= value;
+                }
+            }
+            get
+            {
+                return auflage;
+            }
+        }
+
+        public Buch(string autor, string titel, int auflage)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            Auflage = auflage;
+        }
+    }
+}
