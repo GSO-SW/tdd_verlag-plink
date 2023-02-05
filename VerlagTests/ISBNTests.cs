5e3170af5dadabb65476088305b6d4654d2ae031
c3 <dernamentyp@protonmail.com>
2023-02-06 00:32:15 +0100

    ISBNTests.cs added

---
+++ VerlagTests/ISBNTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Verlag;
+
+namespace VerlagTests
+{
+    [TestClass]
+    public class ISBNtests
+    {
+        [TestMethod]
+        public void ISBN_DarfGesetztWerden()
+        {
+            //Arrange
+            string ISBN = "9783770436163";
+
+            //ACT
+            Buch b = new Buch("autor", "titel");
+            b.ISBN = ISBN;
+
+            //Assert
+            Assert.AreEqual(b.ISBN, ISBN);
+        }
+    }
+}
